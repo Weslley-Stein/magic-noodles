5725a130046bb48b2b25b2cb85d0bf7c6e957dd8
Weslley Stein <weslley.stein@gmail.com>
2021-01-04 12:57:14 -0300

    solving the path

--- script.js
+++ script.js
@@ -9,31 +9,31 @@ const option3 = document.querySelector('#option3');
 const spicyNoodles = () => {
     table.style.display = 'flex'
     th1.innerHTML = 'Soft Hot';
-    option1.src = './img/ramen1.jpg'
+    option1.src = './img/Ramen1.jpg'
     th2.innerHTML = 'Hot';
-    option2.src = './img/ramen2.jpg'
+    option2.src = './img/Ramen2.jpg'
     th3.innerHTML = 'Extremely Hot';
-    option3.src = './img/ramen8.jpg'
+    option3.src = './img/Ramen8.jpg'
         
 };
 
 const tradicionalNoodles = () => {
     table.style.display = 'flex'
     th1.innerHTML = 'Shoyu Soup';
-    option1.src = './img/ramen3.jpg'
+    option1.src = './img/Ramen3.jpg'
     th2.innerHTML = 'Miso Soup';
-    option2.src = './img/ramen4.jpg'
+    option2.src = './img/Ramen4.jpg'
     th3.innerHTML = 'Pork Soup';
-    option3.src = './img/ramen6.jpg'
+    option3.src = './img/Ramen6.jpg'
 };
 
 const specialNoodles = () => {
     table.style.display = 'flex'
     th1.innerHTML = 'Bacon Noodle';
-    option1.src = './img/ramen5.jpg'
+    option1.src = './img/Ramen5.jpg'
     th2.innerHTML = 'Seafood';
-    option2.src = './img/ramen7.jpg'
+    option2.src = './img/Ramen7.jpg'
     th3.innerHTML = 'Surprise ingredients';
-    option3.src = './img/ramen9.jpg'
+    option3.src = './img/Ramen9.jpg'
 
 };
